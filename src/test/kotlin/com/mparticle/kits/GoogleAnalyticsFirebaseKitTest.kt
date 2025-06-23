e011a57d62ce5a2b19f7d1b477a32cb1505ceed2
Mansi Pandya <mpandya@mparticle.com>
2025-06-23 10:54:50 -0400

    fix: Replace mock KitListener with an anonymous implementation in constructor

--- src/test/kotlin/com/mparticle/kits/GoogleAnalyticsFirebaseKitTest.kt
+++ src/test/kotlin/com/mparticle/kits/GoogleAnalyticsFirebaseKitTest.kt
@@ -866,7 +866,14 @@ class GoogleAnalyticsFirebaseKitTest {
         }
 
         override fun getKitListener(): KitListener {
-            return KitListener.EMPTY
+            return object : KitListener {
+                override fun kitFound(kitId: Int) {}
+                override fun kitConfigReceived(kitId: Int, configuration: String?) {}
+                override fun kitExcluded(kitId: Int, reason: String?) {}
+                override fun kitStarted(kitId: Int) {}
+                override fun onKitApiCalled(kitId: Int, used: Boolean?, vararg objects: Any?) {}
+                override fun onKitApiCalled(methodName: String?, kitId: Int, used: Boolean?, vararg objects: Any?) {}
+            }
         }
     }
 }
